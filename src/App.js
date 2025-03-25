0948152d37c3004a54b9dd2a5ab606c373f21bf7
Dawid <dawid.hum1@gmail.com>
2025-03-24 22:49:13 -0300

    Aplicação de lista de tarefas em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', category: "trabalho", isCompleted: false},
+    { id: 2, text: 'Learn React', category: "estudos", isCompleted: false},
+    { id: 3, text: 'Learn React', category: "pessoal", isCompleted: false},
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Lista de tarefas </h1>
+      <div className="todo-list">
+        {todos.map((todo) => (  // Correção aqui
+          <div className="todo" key={todo.id}> {/* Adicionado key={todo.id} */}
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">{todo.category}</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
